ea447f40c0b6ba9a93cca493a5bb32e705fa3168
Hustler-Am-Limit <abudjixtra@gmail.com>
2024-03-30 22:51:07 +0100

    remove dark mode

--- components/AppSearch.vue
+++ components/AppSearch.vue
@@ -212,7 +212,7 @@ watch(Escape, () => {
                             @click="closeButtonHandler"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-6 w-6 text-slate-600 hover:text-slate-800" viewBox="0 0 512 512">
-                                <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
+                                <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
                             </svg>
                         </button>
                     </div>
@@ -278,10 +278,6 @@ watch(Escape, () => {
 }
 .search-result-content-wrapper {
     @apply flex gap-2 rounded p-1 flex-col overflow-hidden;
-}
-
-.dark .search-result.selected {
-    @apply bg-gray-800;
 }
 
 .search-result-content-head {
